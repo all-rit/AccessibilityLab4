826cf346a3d529b31b226e797c50f9b15100089b
cxs7700 <cxs7700@g.rit.edu>
2020-11-25 22:26:10 -0500

    quiz questions and answers

--- client/src/quiz/api/quizQuestions.js
+++ client/src/quiz/api/quizQuestions.js
@@ -1,114 +1,105 @@
 var quizQuestions = [
     {
-        question: "",
+        question: "What is a dexterity/mobility impairment?",
         answers: [
             {
                 val: 0,
-                content: "Test"
+                content: "Inability to hear"
             },
             {
                 val: 1,
-                content: "Test"
+                content: "Loss of fine control of movement"
             },
             {
                 val: 0,
-                content: "Test"
+                content: "Loss of vision"
             },
             {
                 val: 0,
-                content: "Test"
+                content: "Inability to distinguish between certain colors"
             }
 
         ],
         multiChoice: false
     },
     {
-        question: "?",
+        question: "What are some examples of assistive technology for people with dexterity impairments?",
         answers: [
             {
                 val: 0,
-                content: ""
+                content: "Altered keyboards"
             },
             {
                 val: 0,
-                content: ""
+                content: "Voice/speech recognition systems"
+            },
+            {
+                val: 0,
+                content: "On-screen keyboard programs"
             },
             {
                 val: 1,
-                content: ""
-            },
-            {
-                val: 0,
-                content: " "
+                content: "All of the above"
             }
 
         ],
         multiChoice: false
     },
     {
-        question: "?",
+        question: "What is one way to improve web accessibility for people with dexterity/mobility impairments?",
         answers: [
             {
+                val: 0,
+                content: "Use many headings and subheadings"
+            },
+            {
+                val: 0,
+                content: "Ensure that text and interactive elements have a color contrast ratio of at least 4.5:1"
+            },
+            {
+                val: 0,
+                content: "Use audio cues upon completion of an interaction"
+            },
+            {
                 val: 1,
-                content: ""
-            },
-            {
-                val: 0,
-                content: ""
-            },
-            {
-                val: 0,
-                content: ""
-            },
-            {
-                val: 0,
-                content: ""
+                content: "Ensure all functions can be accessed with the keyboard"
             }
 
         ],
         multiChoice: false
     },
     {
-        question: "",
+        question: "A large portion of the elderly community deal with worsening motor capabilities, making it hard for them to use the web.",
         answers: [
             {
-                val: 0,
-                content: ""
-            },
-            {
-                val: 0,
-                content: ""
-            },
-            {
                 val: 1,
-                content: ""
+                content: "True"
             },
             {
                 val: 0,
-                content: ""
+                content: "False"
             }
-
         ],
         multiChoice: false
     },
     {
-        question: " ",
+        question: "Which of the following are challenges that people with dexterity/mobility impairments experience?",
         answers: [
             {
-                val: 0,
-                content: ""
-            },
-            {
                 val: 1,
-                content: ""
-            },
-            {
-                val: 1,
-                content: ""
+                content: "Users may become fatigued when using the assistive technologies"
             },
             {
                 val: 0,
-                content: ""
+                content: "Users are able to physically interact with their hardware device"
+            },
+            {
+                val: 0,
+                content: "Users may become well-adapted to the assistive technologies over time"
+            },
+            {
+                val: 1,
+                content: "Users may not be able to use the mouse or peripherals"
             }
 
         ],
